e110e1844e6b082ed98a74d55d138a4e54ab8923
shadowofthecolossus <ylihayfufuft@gmail.com>
2026-06-09 18:01:31 +0500

    first commit

---
+++ PP2summer/Assignment1/strings/Modify strings.py
@@ -0,0 +1,15 @@
+#Upper Case
+a = "Hello, World!"
+print(a.upper())
+#Lower Case
+b = "Hello, World!"
+print(b.lower())
+#Remove Whitespace
+c = " Hello, World! "
+print(c.strip()) # returns "Hello, World!" 
+#Replace String
+d = "Hello, World!"
+print(d.replace("H", "J"))
+#Split String
+z = "Hello, World!"
+print(z.split(",")) # returns ['Hello', ' World!'] 
---
+++ PP2summer/Assignment1/syntax/Python Variables.py
@@ -0,0 +1,3 @@
+x = 5
+y = "Hello, World!"
+'''Python has no command for declaring a variable.'''
